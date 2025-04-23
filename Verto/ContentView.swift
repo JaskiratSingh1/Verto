429affe89c314843e1693e260c6012d88b623312
Jaskirat Singh <38223354+JaskiratSingh1@users.noreply.github.com>
2025-04-23 14:31:09 -0600

    Initial Commit

---
+++ Verto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Verto
+//
+//  Created by Jaskirat Singh on 2025-04-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Verto/VertoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VertoApp.swift
+//  Verto
+//
+//  Created by Jaskirat Singh on 2025-04-23.
+//
+
+import SwiftUI
+
+@main
+struct VertoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
